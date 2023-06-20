9cafab003bd20e542563261b1db0141c9300280c
dear9144 <dear9144@naver.com>
2023-06-20 10:19:02 +0900

    poppi

--- src/main/resources/static/sql/ocean.sql
+++ src/main/resources/static/sql/ocean.sql
@@ -1,6 +1,6 @@
 -- 스키마
 
-USE quddls6;
+USE ocean;
 
 
 DROP TABLE IF EXISTS CHAT_MESSAGE;
@@ -2309,3 +2309,5 @@ INSERT INTO HASHTAG_CD (CD_NO, HT_NO) VALUES (68, 5);
 
 INSERT INTO USERS_ACCESS VALUES('james@naver.com', '20230501');  -- user1 정상 회원
 INSERT INTO USERS_ACCESS VALUES('john@gmail.com', '20220501');  -- user2 휴면 대상(12개월 이상 로그인 이력 없음)
+
+SHOW DATABASES;
