9ba1f51fa9843e9928bb62d4795d5dba93ac908b
bbooom2 <ma22c0@naver.com>
2023-06-25 16:28:38 +0900

    ora

--- src/main/java/com/gdu/ocean/config/WebMvcConfig.java
+++ src/main/java/com/gdu/ocean/config/WebMvcConfig.java
@@ -1,8 +1,6 @@
 package com.gdu.ocean.config;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.gdu.ocean.intercept.LoginCheckInterceptor;
@@ -11,22 +9,17 @@ import com.gdu.ocean.util.MyFileUtil;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-@Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
-	
-	//field 
+public class WebMvcConfig implements WebMvcConfigurer{
+	 //field
 	private final LoginCheckInterceptor loginCheckInterceptor;
 	private final MyFileUtil myFileUtil;
-
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(loginCheckInterceptor)
-				.excludePathPatterns("/users/out.do", "/index.html"); // 제외할 요청 
-
-	}
 	
 	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/imageLoad/**")
-				.addResourceLocations("file:" + myFileUtil.getSummernoteImagePath() + "/");
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(loginCheckInterceptor)
+			    .addPathPatterns("/users/logout.do");
+     	registry.addInterceptor(loginCheckInterceptor)
+			 		
+		        .excludePathPatterns("/users/out.do"); // 제외할 요청
 	}
 }
